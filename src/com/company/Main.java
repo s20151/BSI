7eb197d800517bb99f2e77ae3681739dacbcb2f7
Artur Piszczatowski <s20487@pjwstk.edu.pl>
2021-11-10 17:44:52 +0100

    Task 1 done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
 package com.company;
 
+import com.company.tasks.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Task1.investment_estimate(18,40000,0.1);
     }
 }
--- src/com/company/tasks/Task1.java
+++ src/com/company/tasks/Task1.java
@@ -1,2 +1,22 @@
-package com.company.tasks;public class Task1 {
+package com.company.tasks;
+
+import java.text.DecimalFormat;
+
+//03_chapter_V_1
+public class Task1 {
+
+    public static void investment_estimate(int years, double goal, double rate) {
+        double investment = goal;
+        DecimalFormat numberFormat = new DecimalFormat("#.00");
+
+        for(int i=0; i<years; i++){
+            investment=investment/(1+rate);
+        }
+
+        rate=rate*100;
+
+        System.out.println("You should invest " + numberFormat.format(investment) + "$ to reach " +
+                numberFormat.format(goal) + "$ in " + years + " years with " + rate + "% interest rate.");
+    }
+
 }
