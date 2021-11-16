e3b0421e7008e9ec04726b5d1ea5755a9e7500cc
s20151 <s20151@pjwstk.edu.pl>
2021-11-16 19:41:46 +0100

    added task 1 JavaDoc

--- src/com/company/tasks/Task1.java
+++ src/com/company/tasks/Task1.java
@@ -4,17 +4,21 @@ import java.text.DecimalFormat;
 
 //03_chapter_V_1
 public class Task1 {
-
+    /**
+     * Counts how much should we invest to achive given goal with given interest rest for given amount of years.
+     *
+     * @param years - amount of years
+     * @param goal -  amount o money we want to achive
+     * @param rate - interest rate percentage 1-100%
+     * @function prints
+     */
     public static void investment_estimate(int years, double goal, double rate) {
         double investment = goal;
         DecimalFormat numberFormat = new DecimalFormat("#.00");
-
         for(int i=0; i<years; i++){
             investment=investment/(1+rate);
         }
-
         rate=rate*100;
-
         System.out.println("You should invest " + numberFormat.format(investment) + "$ to reach " +
                 numberFormat.format(goal) + "$ in " + years + " years with " + rate + "% interest rate.");
     }
