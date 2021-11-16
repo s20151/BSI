5e7cd94f6191816e5b9db6e4e81cc2eb00ea6744
s20151 <s20151@pjwstk.edu.pl>
2021-11-16 19:59:53 +0100

    refactored class structure AGAIN

--- src/com/company/Tasks.java
+++ src/com/company/Tasks.java
@@ -26,5 +26,31 @@ public class Tasks {
                 numberFormat.format(goal) + "$ in " + years + " years with " + rate*100 + "% interest rate.");
     }
 
+    public static void annuityConverter(int initial_age, int goal_age, double investment, double rate){
+        int years = goal_age - initial_age;
+        double annual_income = investment;
+        DecimalFormat numberFormat = new DecimalFormat("#.00");
 
+        annual_income = annual_income + annual_income * rate;
+        annual_income = annual_income - investment;
+        annual_income = annual_income + annual_income * rate;
+
+
+        System.out.println("To equally divide " + numberFormat.format(investment) + "$ for " + years + " years with " +
+                rate*100 + "% interest rate, you will have " + numberFormat.format(annual_income) + " annual income.");
+    }
+
+    public static void annualDepositEstimatate(int years, double goal, double rate) {
+        double annual_deposit = goal;
+        DecimalFormat numberFormat = new DecimalFormat("#.00");
+
+        for(int i=0; i<years; i++){
+            annual_deposit=annual_deposit/(1+rate);
+        }
+
+
+        System.out.println("To reach " + numberFormat.format(goal) + "$ in " +
+                + years + " years with " + rate*100 + "% interest rate, you should be depositing "
+                + numberFormat.format(annual_deposit) + "$ each year.");
+    }
 }
